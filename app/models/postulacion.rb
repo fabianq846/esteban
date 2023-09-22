93853c10301a7f9fc682db943cad526c85ba121c
fabianq846 <fabianq846@gmail.com>
2023-09-22 22:03:40 +0000

    Solo se modifico el modelo de postulaciones

--- app/models/postulacion.rb
+++ app/models/postulacion.rb
@@ -1,14 +1,4 @@
-# test/models/postulacion_test.rb
-require 'test_helper'
-
-class PostulacionTest < ActiveSupport::TestCase
-  def setup
-    @user = User.create(nombre: "Juan", email: "juan@example.com")
-    @oferta = OfertaLaboral.create(titulo: "Ingeniero", descripcion: "Requiere experiencia...")
-  end
-
-  test "usuario puede postularse a oferta laboral" do
-    postulacion = Postulacion.new(user: @user, oferta_laboral: @oferta)
-    assert postulacion.valid?
-  end
+class Postulacion < ApplicationRecord
+  belongs_to :user
+  belongs_to :oferta_laboral
 end
